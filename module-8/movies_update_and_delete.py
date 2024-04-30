33df7f37d21dc0c23bcc63ecb096275cf77a73b1
JavontaYoung <Javonta87@gmail.com>
2024-04-29 21:55:40 -0700

    Insert,Delete,Update

--- module-8/movies_update_and_delete.py
+++ module-8/movies_update_and_delete.py
@@ -10,6 +10,18 @@ config = {
 
 }
 
+def show_films (cursor,title):
+   
+    cursor.execute("select film_name as Name, film_director as Director, genre_name as Genre, studio_name as 'Studio Name' "
+                   "FROM film INNER JOIN genre ON film.genre_id = genre.genre_id INNER JOIN studio ON film.studio_id = studio.studio_id ")
+    films = cursor.fetchall()
+    print("\n -- {} --".format(title))
+    for directors in films:
+        print("Film Name: {}\n Director: {}\n Genre Name ID: {}\n Studio Name: {}\n".format(directors[0],directors[1],directors[2],directors[3]))
+    
+    
+    
+
 try: 
     db =mysql.connector.connect(**config)
 
@@ -29,38 +41,23 @@ except mysql.connector.Error as err:
 
 finally:
     cursor = db.cursor()
-    # inner join query
-    cursor.execute("select film_name as Name, film_director as Director, genre_name as Genre, studio_name as 'Studio Name' from film INNER JOIN genre ON film.genre_id = genre.genre_id INNER JOIN studio ON film.studio_id = studio.studio_id ")
-    director = cursor.fetchall()
-    print(f"-- DISPLAYING FILMS --")
-    for directors in director:
-        print("Film Name: {}\n Director: {}\n Genre Name ID: {}\n Studio Name: {}\n".format(directors[0],directors[1],directors[2],directors[3]))
-             
-def show_films (cursor,films):
-   
-    cursor.execute("INSERT INTO film (film_name,film_releaseDate,film_director,film_runtime,film_id,studio_id,genre_id) VALUES ('Friday','1995',' F. Gary Gray','91',4,8,5")
-    films1 = cursor.fetchall()
-    print("-- DISPLAYING FILMS AFTER INSERT --")
-    for film1 in films:
-        print("Film Name: {}\n Director: {}\n Genre Name ID: {}\n Studio Name: \n".format(film[1], film[2], film[3], film[8]))
+    show_films(cursor, " DISPLAYING FILMS")
 
+
+    cursor.execute("INSERT INTO film  VALUES (4,'Friday','1995','91',' F. Gary Gray',9,3)")
+    show_films(cursor," DISPLAYING FILMS AFTER INSERT")
+    
+   
        
     #update   
-    cursor.execute("UPDATE genre SET genre_id = 3 genre_name = 'Drama' WHERE film_id = 'Alien'")
-    films2 = cursor.fetchall()
-    print("-- DISPLAYING FILMS AFTER UPDATE --")
+    cursor.execute("UPDATE film SET genre_id = 3 WHERE film_name = 'Alien'")
+    show_films(cursor,"-- DISPLAYING FILMS AFTER UPDATE --" )
 
 
     #Delete
     cursor.execute("DELETE FROM film WHERE film_name = 'Gladiator'")
-    films3 = cursor.fetchall()
-    print("-- DISPLAYING FILMS AFTER DELETE --")
-    
-    print("\n -- {} --".format(films))
-   
-    
-    
-       
-db.close()
+    show_films(cursor,"-- DISPLAYING FILMS AFTER DELETE --")
+     
+    db.close()
 
 
